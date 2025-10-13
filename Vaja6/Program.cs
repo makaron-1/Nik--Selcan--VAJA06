6a5a94a9a02927ff89f46b951debab1ee974f9bf
Jaka Koren <jakoren95@gmail.com>
2025-10-13 18:55:09 +0200

    Ustvarjen projekt za Vajo 6

---
+++ Vaja6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace vaja6 {
+
+    public enum Razred { Bojevnik, Carovnik, Menih, Tat };
+
+    class Igralec {
+        public int id;
+        public string u_ime;
+        public string u_geslo;
+        public static int stIg = 1;
+
+        public Igralec() {
+            u_ime = "";
+            u_geslo = "";
+            id = stIg;
+            stIg++;
+        }
+
+        public int Id {
+            get
+            {
+                return id;
+            }
+        }
+    }
+    class vaja { 
+        public static void main(String[] args)
+        {
+
+        }
+    }
+}
